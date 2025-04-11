873717bdebdc1e15cb9db2663c708102e74ec388
vitória0079 <depaulavitoria820@gmail.com>
2025-04-11 10:48:35 -0300

    contexto usuarios

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -11,24 +11,24 @@ namespace CantinaBariri143.Data
         {
         }
 
-        public DbSet<Clientes> Clientes { get; set; }
+        //public DbSet<Clientes> Clientes { get; set; }
         public DbSet<Usuarios> Usuarios { get; set; }
-        public DbSet<Fornecedores> Fornecedores { get; set; }
-        public DbSet<Alimentos> Alimentos { get; set; }
-        public DbSet<Compras> Compras { get; set; }
-        public DbSet<Pedidos> Pedidos { get; set; }
-        public DbSet<Vendas> Vendas { get; set; }
+        //public DbSet<Fornecedores> Fornecedores { get; set; }
+        // public DbSet<Alimentos> Alimentos { get; set; }
+        //public DbSet<Compras> Compras { get; set; }
+        // public DbSet<Pedidos> Pedidos { get; set; }
+        // public DbSet<Vendas> Vendas { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-            builder.Entity<Clientes>().ToTable("Clientes");
+            //builder.Entity<Clientes>().ToTable("Clientes");
             builder.Entity<Usuarios>().ToTable("Usuarios");
-            builder.Entity<Fornecedores>().ToTable("Fornecedores");
-            builder.Entity<Alimentos>().ToTable("Alimentos");
-            builder.Entity<Compras>().ToTable("Compras");
-            builder.Entity<Pedidos>().ToTable("Pedidos");
-            builder.Entity<Vendas>().ToTable("Vendas");
+            //     builder.Entity<Fornecedores>().ToTable("Fornecedores");
+            //     builder.Entity<Alimentos>().ToTable("Alimentos");
+            //     builder.Entity<Compras>().ToTable("Compras");
+            //     builder.Entity<Pedidos>().ToTable("Pedidos");
+            //     builder.Entity<Vendas>().ToTable("Vendas");
         }
 
 
