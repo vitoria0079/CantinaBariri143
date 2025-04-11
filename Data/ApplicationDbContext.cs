8d41a2e50178cd6a506f1a0cf197e4b9e9fa6716
vitória0079 <depaulavitoria820@gmail.com>
2025-04-11 10:39:02 -0300

    contexto

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using CantinaBariri143.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CantinaBariri143.Data
@@ -9,5 +10,27 @@ namespace CantinaBariri143.Data
             : base(options)
         {
         }
+
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Usuarios> Usuarios { get; set; }
+        public DbSet<Fornecedores> Fornecedores { get; set; }
+        public DbSet<Alimentos> Alimentos { get; set; }
+        public DbSet<Compras> Compras { get; set; }
+        public DbSet<Pedidos> Pedidos { get; set; }
+        public DbSet<Vendas> Vendas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Clientes>().ToTable("Clientes");
+            builder.Entity<Usuarios>().ToTable("Usuarios");
+            builder.Entity<Fornecedores>().ToTable("Fornecedores");
+            builder.Entity<Alimentos>().ToTable("Alimentos");
+            builder.Entity<Compras>().ToTable("Compras");
+            builder.Entity<Pedidos>().ToTable("Pedidos");
+            builder.Entity<Vendas>().ToTable("Vendas");
+        }
+
+
     }
 }
