a64f5d5c13f3df516f46b591c78b39f5cfd0ae8d
vitória0079 <depaulavitoria820@gmail.com>
2025-04-11 11:07:14 -0300

    migration usu

---
+++ Models/Usuarios.cs
@@ -0,0 +1,12 @@
+﻿namespace CantinaBariri143.Models
+{
+    public class Usuarios
+    {
+        public Guid UsuariosId { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+
+    }
+}
