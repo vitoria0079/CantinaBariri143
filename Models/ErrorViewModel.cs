96723c1f3e98c27d5ce4c9e519c04f9ac16b0438
vitória0079 <depaulavitoria820@gmail.com>
2025-04-11 09:07:42 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CantinaBariri143.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
